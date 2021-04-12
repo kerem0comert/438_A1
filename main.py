a76d13654d18a9b84e66d4ccf7b75346992d146b
Kerem <kerem0comert@gmail.com>
2021-04-12 18:08:47 +0300

    getTable() from txt

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import re
+
 def encrypt(): pass
 def decrypt(): pass
+
+def getTable():
+    table = []
+    with open("table.txt") as f:
+        for row in [x.strip() for x in f.readlines()]: 
+            """if a line contains any upper case letters at all, take anything 
+            that is between singlequotes"""
+            if any(x.isupper() for x in row): 
+                table.append(re.findall(r"'(.*?)'", row))
+    print(table)
+
 
 
 def menu():
@@ -16,4 +29,5 @@ def menu():
         menu()
         
 if __name__ == "__main__":
-    menu()
+    table = getTable()
+    #menu()
