9fa00dcb628e0c264a34a25ac63525321397a12a
Kerem <kerem0comert@gmail.com>
2021-04-13 12:16:34 +0300

    Add helper functions and complete decrypt first phase

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from itertools import cycle
 
 table = []
 
+def createTable():
+    with open("table.txt") as f:
+        for row in [x.strip() for x in f.readlines()]: 
+            """if a line contains any upper case letters at all, take anything 
+            that is between singlequotes"""
+            if any(x.isupper() for x in row): 
+                table.append(re.findall(r"'(.*?)'", row))
+    return table
+
 def pos(letter): return ascii_lowercase.index(letter)
 
 """itertools' built-in cycle module can create a generator from a given string
@@ -14,11 +23,20 @@ def repeatKey(key, length):
         key += next(letterPool)
     return key
     
+def cutInHalf(text): return text[:len(text)//2], text[len(text)//2:]
+
+def appendEverySecondChar(text, firstHalf, secondHalf): 
+    for index, letter in enumerate(firstHalf):
+        text += firstHalf[index]
+        text += secondHalf[index]
+    return text
     
-def encrypt(plaintext, key): 
+def encrypt(): 
     print("**********Encryption**********")
     print("Encryption Phase-1")
-
+    
+    plaintext, key = getInputs()
+    
     #remove whitespace chars
     plaintext = re.sub(r"\s+", "", plaintext)
     
@@ -26,28 +44,34 @@ def encrypt(plaintext, key):
     if len(plaintext) > len(key): 
         key = repeatKey(key, len(plaintext))
 
-    cipher = ""
+    firstCipher = ""
     for index, _ in enumerate(plaintext):
-        cipher += table[pos(key[index])][pos(plaintext[index])]
-    print(f"Plaintext: {plaintext}\nKey: {key}\nOutput (phase-1): {cipher}\n")
+        firstCipher += table[pos(key[index])][pos(plaintext[index])]
+        
+    print(f"Plaintext: {plaintext}\nKey: {key}\nOutput (phase-1): {firstCipher}\n")
     print("Encryption Phase-2")
-    if len(cipher) % 2: cipher += '0'
+    if len(firstCipher) % 2: 
+        firstCipher += '0'
     
-    firstHalf, secondHalf = cipher[:len(cipher)//2], cipher[len(cipher)//2:]
+    firstHalf, secondHalf = cutInHalf(firstCipher)
+    print(f"Inputtext: {firstCipher}\ngroup-1: {firstHalf}\ngroup-2: {secondHalf}")
     
+    secondCipher = appendEverySecondChar("", firstHalf, secondHalf)
+    print(f"Ciphertext: {secondCipher}")
+    return secondCipher
     
-    
-def decrypt(cipher, key): pass
 
-def createTable():
-    with open("table.txt") as f:
-        for row in [x.strip() for x in f.readlines()]: 
-            """if a line contains any upper case letters at all, take anything 
-            that is between singlequotes"""
-            if any(x.isupper() for x in row): 
-                table.append(re.findall(r"'(.*?)'", row))
-    return table
-
+    
+def decrypt(): 
+    print("**********Decryption**********")
+    print("Decryption Phase-1")
+    
+    cipher, key = getInputs()
+    
+    firstHalf, secondHalf = cutInHalf(cipher)
+    firstPlainText = appendEverySecondChar("", firstHalf, secondHalf)
+    print(firstPlainText)
+    
 
 def getInputs():
     inputText = input("Inputtext: ")
@@ -61,12 +85,8 @@ def menu():
     print("[2] Decrypt")
     print("[3] Exit")
     selection = int(input("Select: "))
-    if selection == 1: 
-        plaintext, key = getInputs()
-        encrypt(plaintext, key)
-    elif selection == 2: 
-        cipher, key = getInputs()
-        decrypt(cipher, key)
+    if selection == 1: encrypt()
+    elif selection == 2: decrypt()
     elif selection == 3: exit()
     else:
         print("Undefined selection!")
